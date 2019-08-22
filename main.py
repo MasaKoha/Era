51b44caa205ee6e51bff61ca4f3b73f1ae06dd4c
Masataka Kohagura <masalinkinpark@gmail.com>
2019-08-22 15:21:42 +0900

    Add

--- main.py
+++ main.py
@@ -3,7 +3,20 @@
 
 
 def main():
-    print('Hello World')
+    file_data = open("log.txt", "r")
+    all_character = ""
+    for line in file_data:
+        all_character += line.encode().decode('unicode-escape')
+    char_list = list(set(all_character))
+
+    write_characters = ""
+    for character in char_list:
+        write_characters += character
+
+    file_writer = open('use_character.txt', 'w')
+    file_writer.write(write_characters)
+    file_writer.close()
+    print("Output File use_character.txt")
 
 
 if __name__ == '__main__':
